e7e934c2d9503f4666c1bf2993200cbc4a3b8d43
mel_arr <37823114+meliarr@users.noreply.github.com>
2023-07-17 17:18:27 -0500

    updates and tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'mainPurple': '#413543',
+				'accentPurple': '#8843E7',
+				'accentGreen': '#E9E486',
+				'black': '#000000',
+				'white': '#ffffff',
+				'neutral': '#F6F6F6',
+				'body': '#2D2727',
+				'link': '#8843E7',
+			},
+			fontFamily: {
+				sans: ['"Nimbus Sans Light"', 'sans-serif'],
+				serif: ['Larken', 'serif'],
+				condensed: ['"Bebas Neue Regular"', 'serif'],
+			},
+		},
+	},
+	plugins: [],
+}

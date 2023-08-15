56eec98507473a227299cd5ce7f3fb22d29bef3f
mel_arr <37823114+meliarr@users.noreply.github.com>
2023-08-14 19:41:38 -0500

    installing react and lightbox.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://melarroyo.com',
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), react()]
 });

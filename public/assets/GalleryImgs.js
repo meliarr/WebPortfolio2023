23c779ae9a16bc57c205fc68ca5ba2883210673b
mel_arr <37823114+meliarr@users.noreply.github.com>
2023-09-03 13:38:20 -0500

    Import galleryImgs array into ArtGallery section

---
+++ public/assets/GalleryImgs.js
@@ -0,0 +1,32 @@
+const galleryImgs = [
+    {
+        src: '/assets/portfolio-avobites2.png',
+        alt: 'Illustration of an avocado and rice bowl with ingredients floating on top of each other and the word Mexicano on the bowl'
+    },
+    {
+        src: '/assets/portfolio-avobites3.png',
+        alt: 'Illustration of an avocado and rice bowl with ingredients floating on top of each other'
+    },
+    {
+        src: '/assets/portfolio-cnc-enjoy.png',
+        alt: 'Illustration of two garnished cocktails with the word enjoy on one glass, and the words html, css, js, and git on the ice cubes in the other glass'
+    },
+    {
+        src: '/assets/portfolio-cnc-html.png',
+        alt: 'Illustration of an empty cocktail glass with a banner that says The Glass HTML'
+    },
+    {
+        src: '/assets/portfolio-cnc-javascript.png',
+        alt: 'Illustration of a liquor bottle and a shotglass with the letters JS on the label and the words The Liquor next to the bottle'
+    },
+    {
+        src: '/assets/portfolio-personal1.png',
+        alt: 'Line portrait of a woman with short hair and sunglasses'
+    },
+    {
+        src: '/assets/portfolio-personal2.png',
+        alt: 'Line portrait of a woman with long hair in a ponytail and boxing gloves'
+    },
+];
+
+export { galleryImgs };
